54a398c8ba1d122bf4419e3f8be49cf925437d48
Ulianova <izzzya1917@gmail.com>
2023-08-02 11:39:44 +0500

    крутится

--- app/src/main/java/com/izzzya/fortunewheel/WheelFragment.kt
+++ app/src/main/java/com/izzzya/fortunewheel/WheelFragment.kt
@@ -1,15 +1,28 @@
 package com.izzzya.fortunewheel
 
+import android.animation.Animator.AnimatorListener
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.Animation
+import android.view.animation.DecelerateInterpolator
+import android.view.animation.RotateAnimation
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
+import java.util.Random
 
 
 class WheelFragment : Fragment() {
+    private val sectors = arrayOf<Int>(45, 25, 750, 450, 200, 125, 100, 75)
+    private var sectorDegrees = IntArray(sectors.size)
+    private var rndSectorIndex = 0
+    var spinning = false
+
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,9 +41,58 @@ class WheelFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val spinBtn = view.findViewById<Button>(R.id.spinBtn)
+        val earned = 0
+        val wheel = view.findViewById<ImageView>(R.id.wheelIV)
+        generateSectorDegrees()
+
+        fun spin() {
+            rndSectorIndex = Random().nextInt(sectors.size)
+            val rndDegree = generateRandomDegree()
+            val anim = RotateAnimation(0f, rndDegree.toFloat(), RotateAnimation.RELATIVE_TO_SELF,
+                0.5f, RotateAnimation.RELATIVE_TO_SELF,
+                0.5f)
+            anim.duration = 3600
+            anim.fillAfter = true
+            anim.interpolator = DecelerateInterpolator()
+            anim.setAnimationListener(object: Animation.AnimationListener{
+                override fun onAnimationStart(p0: Animation?) {
+                }
+
+                override fun onAnimationEnd(p0: Animation?) {
+                    val earnedCoins = sectors[sectors.size-(rndSectorIndex-1)]
+                    Toast.makeText(requireContext(), earnedCoins.toString(), Toast.LENGTH_SHORT).show()
+                    spinning = false
+                    findNavController().navigate(R.id.action_global_questionFragment)
+
+                }
+
+                override fun onAnimationRepeat(p0: Animation?) {
+                }
+
+            })
+
+            wheel.startAnimation(anim)
+        }
 
         spinBtn.setOnClickListener(){
-            findNavController().navigate(R.id.action_global_questionFragment)
+            if (!spinning){
+                spin()
+                spinning = true
+            }
+
+        }
+    }
+
+
+
+    private fun generateRandomDegree():Int {
+        return (360*sectors.size) + sectorDegrees[rndSectorIndex]
+    }
+
+    private fun generateSectorDegrees() {
+        val sectorDegree = 360/sectors.size
+        for (i in 0..sectors.size-1){
+            sectorDegrees[i] = (i+1)*sectorDegree
         }
     }
 
